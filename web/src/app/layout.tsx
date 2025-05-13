98d8326cdd7cac6282a47c04f92f307039beab01
Rxbru <amarmahato.cs@gmail.com>
2025-05-13 20:02:08 +0530

    just issues

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -6,15 +6,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Major+Mono+Display&family=Oswald:wght@200;300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head> 
+    <html lang="en"> 
       <body>{children}</body>
     </html>
   );
