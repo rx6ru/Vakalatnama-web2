a6fae1d3f4bafa98a8b6dc9bb6befa33cde290d4
Rxbru <amarmahato.cs@gmail.com>
2025-05-15 02:44:32 +0530

    I hate this

---
+++ server/src/config.ts
@@ -0,0 +1,5 @@
+export default {
+    JWT_SECRET: process.env.JWT_SECRET,
+    SUPABASE_URL: process.env.SUPABASE_URL
+}
+
--- server/src/middleware/petAuth.ts
+++ server/src/middleware/petAuth.ts
@@ -1,8 +1,26 @@
 //authcheck middleware it is a ts file so add types too
 
 import { Request, Response, NextFunction } from 'express';
+//impportjwt in ts
+import {verify, JwtPayload} from 'jsonwebtoken';
+import config from '../config';
+
 
 export default function authCheck(req: Request, res: Response, next: NextFunction) {
     // your auth check logic
+    const token = req.headers.authorization?.split(' ')[1];
+    
+    if (!token) {
+        res.status(401).json({ message: 'Unauthorized' });
+    }else{
+        // Verify token
+        try {
+            const decoded = verify(token, config.JWT_SECRET!) as JwtPayload;
+            req.headers.username = decoded.username;
+        } catch (error) {
+            res.status(401).json({ message: 'Invalid token' });
+        }
+    }
+    
     next();
 }
